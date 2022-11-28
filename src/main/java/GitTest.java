46e066ee2466cea1938873b00f096123027c71a2
itcast <you@example.com>
2022-11-28 20:52:56 +0800

    123. ..

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,10 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("123....");
+        int i = 0;
+        for (int j = 0;j<10;j++){
+            i+=j;
+        }
+        System.out.println(i);
     }
 }
